2f88986810570e77b5b23005524d5050381d0e96
Elizabeth Karr <e.karr93@icloud.com>
2023-02-21 11:13:03 -0600

    add script for changing grid size with prompt button

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 let gridContainer = document.querySelector('.grid-container');
 let resetButton = document.querySelector('.reset');
 let promptButton = document.querySelector('.grid-size');
-let gridSize = 0;
+let gridSize = 16;
+
+createGrid(gridSize);
 
 promptButton.addEventListener('click', () => {
     gridSize = Number(prompt("How many squares per side?"));
-});
-
-for (let i = 0; i < 25; i++) {
-    let rowDiv = document.createElement('div');
-    rowDiv.setAttribute('style', 'display: flex');
-    for (let j = 0; j < 25; j++) {
-        let block = document.createElement('div');
-        block.setAttribute('style', 'border: 1px solid grey;');
-        block.style.height = ((gridContainer.clientHeight / 25) - 2) + "px";
-        block.style.width = ((gridContainer.clientWidth / 25) - 2) + "px";
-        block.classList.add('block');
-        rowDiv.appendChild(block);
+    let rows = document.querySelectorAll('.row');
+    for (let i = 0; i < rows.length; i++) {
+        gridContainer.removeChild(rows[i]);
     }
-    gridContainer.appendChild(rowDiv);
-}
+    createGrid(gridSize);
+});
 
 let gridBlocks = document.querySelectorAll('.block');
 
@@ -36,5 +29,22 @@ function colorGrid(e) {
 function reset(item) {
     for (let i = 0; i < item.length; i++) {
         item[i].style.backgroundColor = "white";
+    }
+}
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.setAttribute('style', 'display: flex');
+        for (let j = 0; j < size; j++) {
+            let block = document.createElement('div');
+            block.setAttribute('style', 'border: 1px solid grey;');
+            block.style.height = ((gridContainer.clientHeight / size) - 2) + "px";
+            block.style.width = ((gridContainer.clientWidth / size) - 2) + "px";
+            block.classList.add('block');
+            rowDiv.appendChild(block);
+        }
+        gridContainer.appendChild(rowDiv);
     }
 }
