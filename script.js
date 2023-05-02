34b2fc257a28c9149201f57b45988fa0f459c48a
Elizabeth Karr <e.karr93@icloud.com>
2023-05-02 17:54:18 -0500

    install eslint

--- script.js
+++ script.js
@@ -1,102 +1,100 @@
-let gridContainer = document.querySelector('.grid-container');
-let resetButton = document.querySelector('.reset');
-let gridSizeButtons = document.querySelectorAll('.grid-size');
-let colorButtons = document.querySelectorAll('.color');
-let blackButton = document.querySelector('#black');
-let rainbowButton = document.querySelector('#rainbow');
-let gridSize = 15;
+const gridContainer = document.querySelector('.grid-container');
+const resetButton = document.querySelector('.reset');
+const gridSizeButtons = document.querySelectorAll('.grid-size');
+const colorButtons = document.querySelectorAll('.color');
+const blackButton = document.querySelector('#black');
+const rainbowButton = document.querySelector('#rainbow');
+const gridSize = 15;
 
 window.addEventListener('load', () => createGrid(gridSize));
 
-gridSizeButtons.forEach(button => button.addEventListener('click', () => {
+gridSizeButtons.forEach((button) => button.addEventListener('click', () => {
+  const rows = document.querySelectorAll('.row');
+  for (let i = 0; i < rows.length; i++) {
+    gridContainer.removeChild(rows[i]);
+  }
 
-    let rows = document.querySelectorAll('.row');
-    for (let i = 0; i < rows.length; i++) {
-        gridContainer.removeChild(rows[i]);
-    }
+  for (let i = 0; i < gridSizeButtons.length; i++) {
+    gridSizeButtons[i].classList.remove('selected');
+  }
 
-    for (let i = 0; i < gridSizeButtons.length; i++) {
-        gridSizeButtons[i].classList.remove('selected');
-    }
+  button.classList.add('selected');
 
-    button.classList.add('selected');
+  blackButton.click();
 
-    blackButton.click();
-
-    createGrid(button.value);
+  createGrid(button.value);
 }));
 
-colorButtons.forEach(button => button.addEventListener('click', () => {
-    
-    for (let i = 0; i < colorButtons.length; i++) {
-        colorButtons[i].classList.remove('selected');
-    }
+colorButtons.forEach((button) => button.addEventListener('click', () => {
+  for (let i = 0; i < colorButtons.length; i++) {
+    colorButtons[i].classList.remove('selected');
+  }
 
-    button.classList.add('selected');
+  button.classList.add('selected');
 }));
 
 rainbowButton.addEventListener('click', () => {
-    createColoring('rainbow');
+  createColoring('rainbow');
 });
 
 blackButton.addEventListener('click', () => {
-    createColoring('black');
+  createColoring('black');
 });
 
 function createColoring(color) {
-    let gridBlocks = document.querySelectorAll('.block');
-    gridBlocks.forEach(block => block.addEventListener('mouseover', () => {
-        if (color === 'black') {
-            block.style.backgroundColor = color;
-        } else {
-            block.style.backgroundColor = randomRgbColor();
-        }
-    }));
+  const gridBlocks = document.querySelectorAll('.block');
+  gridBlocks.forEach((block) => block.addEventListener('mouseover', () => {
+    if (color === 'black') {
+      block.style.backgroundColor = color;
+    } else {
+      block.style.backgroundColor = randomRgbColor();
+    }
+  }));
 }
 
 function resetGrid(item) {
-    for (let i = 0; i < item.length; i++) {
-        item[i].style.backgroundColor = "white";
-    }
+  for (let i = 0; i < item.length; i++) {
+    item[i].style.backgroundColor = 'white';
+  }
 }
 
 function makeBlocks(size) {
-    for (let i = 0; i < size; i++) {
-        let rowDiv = document.createElement('div');
-        rowDiv.classList.add('row');
-        rowDiv.setAttribute('style', 'display: flex');
-        for (let j = 0; j < size; j++) {
-            let block = document.createElement('div');
-            block.setAttribute('style', 'border: 1px solid grey; background-color: white');
-            block.style.height = ((gridContainer.clientHeight / size) - 2) + "px";
-            block.style.width = ((gridContainer.clientWidth / size) - 2) + "px";
-            block.classList.add('block');
-            rowDiv.appendChild(block);
-        }
-        gridContainer.appendChild(rowDiv);
+  for (let i = 0; i < size; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    rowDiv.setAttribute('style', 'display: flex');
+    for (let j = 0; j < size; j++) {
+      const block = document.createElement('div');
+      block.setAttribute('style', 'border: 1px solid grey; background-color: white');
+      block.style.height = `${(gridContainer.clientHeight / size) - 2}px`;
+      block.style.width = `${(gridContainer.clientWidth / size) - 2}px`;
+      block.classList.add('block');
+      rowDiv.appendChild(block);
     }
+    gridContainer.appendChild(rowDiv);
+  }
 }
 
 function createGrid(size) {
-    makeBlocks(size);
+  makeBlocks(size);
 
-    let gridBlocks = document.querySelectorAll('.block');
+  const gridBlocks = document.querySelectorAll('.block');
 
-    createColoring('black');
+  createColoring('black');
 
-    resetButton.addEventListener('click', () => {
-        resetGrid(gridBlocks);
-    });
+  resetButton.addEventListener('click', () => {
+    resetGrid(gridBlocks);
+  });
 }
 
 function randomInteger(max) {
-    return Math.floor(Math.random() * (max + 1));
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function randomRgbColor() {
-    let r = randomInteger(255);
-    let g = randomInteger(255);
-    let b = randomInteger(255);
+  const r = randomInteger(255);
+  const g = randomInteger(255);
+  const b = randomInteger(255);
 
-    return `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
 }
